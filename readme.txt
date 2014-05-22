f1d55ce05455df5cd57374a0eb8272339c8b2e69
anoopranawat <anoopranawat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-22 04:23:03 +0000

    added main files
    git-svn-id: https://plugins.svn.wordpress.org/sp-woocommerce-best-selling-products-by-category/trunk@919146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== sp woocommerce best selling products by category  ===
+Contributors: sptechnolab, anoopranawat 
+Tags: woocommerce, best selling products , best selling products by category, shortcode , template code
+Requires at least: 3.1
+Tested up to: 3.9.1
+Author URI: http://www.sptechnolab.com
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A quick, easy way to add an best selling products by category to woocommerce.
+
+== Description ==
+
+This is very simple plugin to display woocommerce best selling products by category
+
+View [Shortcodes included with WooCommerce](http://docs.woothemes.com/document/woocommerce-shortcodes/) for additional information.
+
+In this plugin we have just changes the best selling shortcode.
+
+The default short code is : <code>[best_selling_products per_page="12"] </code>
+And change in this shortcode with the help of best selling products by category plugin is : <code>[bestselling_product_categories cats="CATEGORY_ID" per_cat="6" columns="3"] </code>
+
+With this plugin you can now display woocommerce best selling products by category.
+
+= Features include: =
+* Best selling products by category
+* Easy to configure
+* Smoothly integrates into any woocommerce theme
+ 
+== Installation ==
+
+1. Upload the 'sp-woocommerce-best-selling-products-by-category' folder to the '/wp-content/plugins/' directory.
+2. Activate the sp-woocommerce-best-selling-products-by-category plugin through the 'Plugins' menu in WordPress.
+3. Use the shortcode <code>[bestselling_product_categories cats="CATEGORY_ID" per_cat="6" columns="3"] </code> to display the best selling products by category
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. Best selling products by category view
+2. How to add shortcode
+
+== Changelog ==
+
+= 1.0 =
+* best selling products by category
+
+
+== Upgrade Notice ==
+
+
+
---
+++ sp-woocommerce-best-selling-products-by-category.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: sp woocommerce best selling products by category
+Plugin URL: http://sptechnolab.com
+Description: Display woocommerce best selling products by category
+Version: 1.0
+Author: SP Technolab
+Author URI: http://www.sptechnolab.com
+Contributors: SP Technolab
+*/
+
+// get bestselling_product_categories
+
+
+add_shortcode( 'bestselling_product_categories', 'sp_bestselling_products' );
+function sp_bestselling_products($atts){
+ 
+	global $woocommerce_loop;
+ 
+	extract(shortcode_atts(array(
+		'cats' => '',	
+		'tax' => 'product_cat',	
+		'per_cat' => '3',	
+		'columns' => '3',	
+	), $atts));
+ 
+	
+	if(empty($cats)){
+		$terms = get_terms( 'product_cat', array('hide_empty' => true, 'fields' => 'ids'));
+		$cats = implode(',', $terms);
+	}
+ 
+	
+	$cats = explode(',', $cats);
+ 
+	
+	if(empty($cats)){
+		return '';
+	}
+ 
+	ob_start();
+ 
+	foreach($cats as $cat){
+ 
+		// get the product category
+		$term = get_term( $cat, $tax);
+ 
+		// setup query
+		$args = array(
+			'post_type' 			=> 'product',
+			'post_status' 			=> 'publish',
+			'ignore_sticky_posts'   => 1,
+			'posts_per_page'		=> $per_cat,			
+			'meta_key' 		 		=> 'total_sales',
+			'orderby' 		 		=> 'meta_value_num',
+			'tax_query' => array(				
+				array(
+					'taxonomy' => $tax,
+					'field' => 'id',
+					'terms' => $cat,
+				)
+			),
+			'meta_query' 			=> array(
+				array(
+					'key' 		=> '_visibility',
+					'value' 	=> array( 'catalog', 'visible' ),
+					'compare' 	=> 'IN'
+				)
+			)
+		);
+
+		// set woocommerce columns
+		$woocommerce_loop['columns'] = $columns;
+ 
+		// query database
+		$products = new WP_Query( $args );
+ 
+		$woocommerce_loop['columns'] = $columns;
+ 
+		if ( $products->have_posts() ) : ?>
+ 
+			
+			<?php woocommerce_product_loop_start(); ?>
+ 
+				<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+ 
+					<?php woocommerce_get_template_part( 'content', 'product' ); ?>
+ 
+				<?php endwhile; // end of the loop. ?>
+ 
+			<?php woocommerce_product_loop_end(); ?>
+ 
+		<?php endif;
+ 
+		wp_reset_postdata();
+	}
+ 
+	return '<div class="woocommerce columns-' . $columns . '">' . ob_get_clean() . '</div>';
+}
+?>
